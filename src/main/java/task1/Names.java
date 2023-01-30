f68082a987c1c436d3eb4248ddb78b22b4713834
imykhailychenko <igor.c.m@ukr.net>
2023-01-30 22:23:57 +0100

    update

--- src/main/java/task1/Names.java
+++ src/main/java/task1/Names.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
 public class Names {
     public String format(final String[] names) {
         return IntStream.range(0, names.length)
-               .filter(i -> (i + 1) % 2 != 0)
+                .filter(i -> (i + 1) % 2 != 0)
                 .mapToObj(i -> (i + 1) + ". " + names[i])
                 .collect(Collectors.joining(", "));
     }
